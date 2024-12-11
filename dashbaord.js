05f8005ba1e6a6d47bf93586824b1f71d2bc56f6
Mandalorian7773 <adityajagrani8@gmail.com>
2024-12-11 22:32:04 +0530

    sellers page

--- dashbaord.js
+++ dashbaord.js
@@ -23,3 +23,5 @@ document.addEventListener("DOMContentLoaded", () => {
         // Add promo code handling logic here
     });
 });
+
+
---
+++ seller.js
@@ -0,0 +1,12 @@
+document.getElementById('seller').addEventListener('submit' , function() {
+    event.defaultPrevented();
+
+    const modelName = document.getElementById('img').value;
+
+    const newElement = document.createElement('div');
+
+
+
+});
+
+
